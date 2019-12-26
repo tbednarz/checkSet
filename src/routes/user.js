a62058ad02736b9d249233b01bc108a5f03209a3
Tres <tresbednarz@gmail.com>
2019-12-26 16:51:14 -0500

    stuff

--- src/routes/user.js
+++ src/routes/user.js
@@ -13,7 +13,7 @@ router.post("/users", async (req, res) => {
     res.status(400).send(e);
   }
 });
-
+//login user
 router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findbyCredentials(
@@ -26,5 +26,30 @@ router.post("/users/login", async (req, res) => {
     res.status(400).send(e);
   }
 });
+//get user
+router.get("/users/:id", async (req, res) => {
+  const _id = req.params.id;
+  try {
+    const user = await User.findById(_id);
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (error) {
+    res.status(500).send(error);
+  }
+});
 
+router.delete("/users/:id", async (req, res) => {
+  try {
+    const user = await User.findByIdAndDelete(req.params.id);
+
+    if (!user) {
+      return res.status(404).send();
+    }
+    res.send(user);
+  } catch (e) {
+    res.status(500).send();
+  }
+});
 module.exports = router;
