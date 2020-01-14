880d10e31c167ae2dfc8a86493e6af8426ab0893
Tres <tresbednarz@gmail.com>
2020-01-14 14:17:13 -0500

    documenting routers

--- src/routes/user.js
+++ src/routes/user.js
@@ -3,7 +3,14 @@ const router = new express.Router();
 const User = require("../models/user");
 const auth = require("../middleware/auth");
 
-//create User
+/* 
+  CREATE A USER
+  create new user from User model using passed in body
+  save the user to database
+  create authorization token using generateAuthToken() from userSchema
+  if successful return 201 created code, send user and token data
+  if fail return 400 status and error
+  */
 router.post("/users", async (req, res) => {
   const user = new User(req.body);
   try {
@@ -15,7 +22,12 @@ router.post("/users", async (req, res) => {
     res.status(400).send(e);
   }
 });
-
+/*
+    LOGIN USER
+    Find user using passed in email and password,
+    create Authorization token
+    send the user and token.
+*/
 router.post("/users/login", async (req, res) => {
   try {
     const user = await User.findByCredentials(
@@ -28,6 +40,14 @@ router.post("/users/login", async (req, res) => {
     res.status(400).send();
   }
 });
+
+/* 
+    LOGOUT USER
+    try to filter out all user tokens that dont equal passed in token
+    save user to record new tokens
+    send
+    if failed send 500 status 
+*/
 router.post("/users/logout", auth, async (req, res) => {
   try {
     req.user.tokens = req.user.tokens.filter(token => {
@@ -40,11 +60,21 @@ router.post("/users/logout", auth, async (req, res) => {
     res.status(500).send();
   }
 });
-
+/*
+  GET USER PROFILE
+ the function requires authentication, therefore you have to be logged in
+  request the information from the current user
+*/
 router.get("/users/me", auth, async (req, res) => {
   await res.send(req.user);
 });
-
+/*
+    DELETE USER AND DATA
+    must be authorized
+    use remove() method from mongoose
+    send the user that was deleted
+    if fail send status 500
+*/
 router.delete("/users/me", auth, async (req, res) => {
   try {
     await req.user.remove();
